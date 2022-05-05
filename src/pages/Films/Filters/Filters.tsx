224bc119f0c3b3a2205a6928864d446bbc11fc05
BenoitSafari <gregory.michalak.192@gmail.com>
2022-05-05 18:54:06 +0200

    <style> Filters component responsive OK

--- src/pages/Films/Filters/Filters.tsx
+++ src/pages/Films/Filters/Filters.tsx
@@ -2,14 +2,22 @@ import styles from './Filters.module.scss';
 
 function Filters() {
   return(
-    <div className={`${styles.filters} container`}>
+    <div className={`container ${styles.filters}`}>
       {/* // TODO : Use Button component */}
       <button style={{
         height: '33px', width: '151px', fontSize: '14px', fontWeight: 'medium',
         color: 'white', borderRadius: '15px', border: 'white solid 1px', background: 'none'
-      }}
+      }}      
       >Tout les films</button>
-      <input type='text'/>
+      
+      <div className={styles['filters__search-bar']}>
+        <img src='images/input_search.svg' alt='' />
+        <input 
+          className={styles['filters__search-bar__input']}
+          placeholder='Filtrer par titre'
+          type='text'
+        />
+      </div>
     </div>
   );
 }

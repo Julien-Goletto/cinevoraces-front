81c27b3cb2ac6a8141e9776511b58baa331e9911
BenoitSafari <gregory.michalak.192@gmail.com>
2022-05-05 12:42:56 +0200

    <feature> Footer component + responsive

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,10 +1,12 @@
 import Header from './Header/Header';
+import Footer from './Footer/Footer';
 
 function Layout({children}: Layout) {
   return(
     <>
       <Header/>
       {children}
+      <Footer/>
     </>
   );
 }

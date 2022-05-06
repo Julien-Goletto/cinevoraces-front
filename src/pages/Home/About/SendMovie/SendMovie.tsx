a57a16df50f516ff4cc1de3ddcbc20af36276fc1
anthonyespirat <jayshu666@gmail.com>
2022-05-06 15:36:39 +0200

    <fix> UnBem-Bem

--- src/pages/Home/About/SendMovie/SendMovie.tsx
+++ src/pages/Home/About/SendMovie/SendMovie.tsx
@@ -3,13 +3,13 @@ import styles from './SendMovie.module.scss';
 function SendMovie() {
   return (
     <section className={styles.send}>
-      <div className={styles.send__wrapper}>
-        <h2 className={`${styles['send__title']} ${styles['send__title--top']}`}>Chaque semaine, une découverte !</h2>
-        <div className={styles.send__content}>
+      <div className={styles.wrapper}>
+        <h2 className={`${styles['title']} ${styles['title--top']}`}>Chaque semaine, une découverte !</h2>
+        <div className={styles.content}>
 
-          <img src='images/week-movie.png' alt='' className={styles.send__img} />
-          <div className={styles.send__text}>
-            <h2 className={`${styles['send__title']} ${styles['send__title--bottom']}`}>Chaque semaine, une découverte !</h2>
+          <img src='images/week-movie.png' alt='' className={styles.img} />
+          <div className={styles.text}>
+            <h2 className={`${styles['title']} ${styles['title--bottom']}`}>Chaque semaine, une découverte !</h2>
             <p style={{marginBottom: '1rem'}}>
             Une fois par semaine, un membre du ciné-club propose un film à la communauté.
             </p>
--- src/pages/Home/Hero/Hero.tsx
+++ src/pages/Home/Hero/Hero.tsx
@@ -4,15 +4,15 @@ import styles from './Hero.module.scss';
 function Hero() {
   return (
     <section className={styles.hero}>
-      <div className={styles.hero__text}>
-        <h1 className={styles.hero__title}>Bienvenue dans le <span>ciné-club</span> virtuel !</h1>
-        <p className={styles.hero__subtitle}>Chaque semaine, un film à découvrir</p>
-        <div className={styles.hero__btns}>
+      <div className={styles.text}>
+        <h1 className={styles.title}>Bienvenue dans le <span>ciné-club</span> virtuel !</h1>
+        <p className={styles.subtitle}>Chaque semaine, un film à découvrir</p>
+        <div className={styles.btns}>
           <Button state='full'>Découvrir un film</Button>
           <Button state='empty'>S'inscrire</Button>
         </div>
       </div>
-      <div className={styles.hero__img}>
+      <div className={styles.img}>
         <img src='images/hero-img.jpg' alt='Affiche de films' />
       </div>
     </section>

084bad4a54a585eb73e34d1ac09243e1de26268d
anthonyespirat <jayshu666@gmail.com>
2022-05-07 09:48:38 +0200

    <fix> remove bem bem

--- src/components/Layout/Footer/Footer.tsx
+++ src/components/Layout/Footer/Footer.tsx
@@ -3,12 +3,12 @@ import styles from './Footer.module.scss';
 function Footer() {
   return(
     <footer className={`container ${styles.footer}`}>
-      <img className={styles.footer__img} src='images/logo_title.svg' alt='Logo du site' />
-      <div className={styles.footer__links}>
+      <img className={styles.img} src='images/logo_title.svg' alt='Logo du site' />
+      <div className={styles.links}>
         <a href='#' className={styles.link}>Politique de confidentialité</a>
         <a href='#' className={styles.link}>L'équipe</a>
       </div>
-      <div className={styles.footer__copyright}>© Aucun droit réservé - 2022</div>
+      <div className={styles.copyright}>© Aucun droit réservé - 2022</div>
     </footer>
   );
 }

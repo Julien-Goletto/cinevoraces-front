69964f8652482e445b2076fc316d9f9367582cb6
BenoitSafari <gregory.michalak.192@gmail.com>
2022-05-09 10:43:25 +0200

    <style> MovieGrid

--- src/pages/Films/Films.tsx
+++ src/pages/Films/Films.tsx
@@ -1,10 +1,14 @@
+import Button from 'components/Buttons/Button';
 import styles from 'Films.module.scss';
 import Filters from './Filters/Filters';
+import MoviesGrid from './MoviesGrid/MoviesGrid';
 
 function Films() {
   return(
     <>
       <Filters/>
+      <MoviesGrid/>
+      <Button state='empty'>Voir les films suivants (77)</Button>
     </>
   );
 }

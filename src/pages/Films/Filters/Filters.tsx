46fc9909a13a7cbee3966eeabfef2c23398a42b9
BenoitSafari <gregory.michalak.192@gmail.com>
2022-05-09 11:17:01 +0200

    <fix> changed default button to component

--- src/pages/Films/Filters/Filters.tsx
+++ src/pages/Films/Filters/Filters.tsx
@@ -1,30 +1,34 @@
 import { useState } from 'react';
 import DropDownMenu from '../DropDownMenu/DropDownMenu';
+import Button from 'components/Buttons/Button';
 import styles from './Filters.module.scss';
 
 function Filters() {
   const [isDropMenu, SetIsDropMenu] = useState(false);
+  const handleDropDownState = () => {
+    (isDropMenu) ? SetIsDropMenu(false) : SetIsDropMenu(true);
+  };
   
   return(
     <div className={styles.filters}>
-      {/* // TODO : Use Button component */}
-      <button style={{
-        height: '33px', width: '151px', fontSize: '14px', fontWeight: 'medium',
-        color: 'white', borderRadius: '15px', border: 'white solid 1px', background: 'none'
-      }}
-      onClick={() => {
-        (isDropMenu) ? SetIsDropMenu(false) : SetIsDropMenu(true);
-      }}
-      >Tout les films</button>
-      
-      <div className={styles['filters-search-bar']}>
+      <Button 
+        state='empty'
+        action={handleDropDownState}
+      >
+        Filtrer
+        { (isDropMenu) ?
+          <img src='images/filter-open.svg' alt='' />
+          :
+          <img src='images/filter-closed.svg' alt='' />
+        }
+      </Button>
+      <div className={styles['search-bar']}>
         <img src='images/input_search.svg' alt='' />
         <input 
           placeholder='Filtrer par titre'
           type='text'
         />
       </div>
-
       { isDropMenu && <DropDownMenu/> }
     </div>
   );

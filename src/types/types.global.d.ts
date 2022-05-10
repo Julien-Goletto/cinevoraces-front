f8b2ed317bcc9672da57a375f3a27a6a739036b6
BenoitSafari <gregory.michalak.192@gmail.com>
2022-05-10 18:21:34 +0200

    <fix> added conditional 'HTMLTextAreaElement' type to 'onChangeEvent'

--- src/types/types.global.d.ts
+++ src/types/types.global.d.ts
@@ -3,7 +3,7 @@ interface Layout {
 };
 
 type mouseEvent = import('react').MouseEvent<HTMLButtonElement | HTMLInputElement>;
-type onChangeEvent = import('react').ChangeEvent<HTMLInputElement>
+type onChangeEvent = import('react').ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
 type onChangeEventHandler = import('react').ChangeEventHandler<HTMLInputElement>
 type onChangeFormEvent = import('react').FormEvent<HTMLFormElement>
 type Button = {

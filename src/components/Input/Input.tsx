436dc2e4995d7a072e606e2c84922d0785f4a505
BenoitSafari <gregory.michalak.192@gmail.com>
2022-05-10 11:36:01 +0200

    <feature> added a RegEx function to Input Name prop

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -2,13 +2,27 @@ import './Input.module.scss';
 import styles from './Input.module.scss';
 
 function Input({label, name, type, placeholder}: FieldsetTextInput) {
+  const classResolver = (searchedString: string) => {
+    if (RegExp(`\\b${searchedString}\\b`).test(name)) {
+      return searchedString;
+    } else {
+      return name;
+    }
+  };
+  const className = classResolver('password');
 
   return (
     <div className={styles.container}>
       { label &&
         <label className= {styles.label} htmlFor={name}>{label}</label>
       }
-      <input className={`${styles.input} ${styles[`input--${name}`]} `} type={type} placeholder={placeholder} name={name} />
+      <input 
+        className={`
+          ${styles.input} 
+          ${styles[`input--${className}`]} 
+        `} 
+        type={type} placeholder={placeholder} name={name}
+      />
     </div>
   );
 }

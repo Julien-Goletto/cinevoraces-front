a65933ee37c3ed98322eb4774576a1fae1bbe2c4
BenoitSafari <gregory.michalak.192@gmail.com>
2022-05-11 22:51:05 +0200

    <fix> commented unused links on footer

--- src/components/Layout/Footer/Footer.tsx
+++ src/components/Layout/Footer/Footer.tsx
@@ -6,8 +6,9 @@ function Footer() {
       <footer className={styles.footer}>
         <img className={styles.img} src='images/logo_title.svg' alt='Logo du site' />
         <div className={styles.links}>
-          <a href='#' className={styles.link}>Politique de confidentialité</a>
-          <a href='#' className={styles.link}>L'équipe</a>
+          {/* TODO: Issue #61 - #62 */}
+          {/* <a className={styles.link}>Politique de confidentialité</a> */}
+          {/* <a className={styles.link}>L'équipe</a> */}
         </div>
         <div className={styles.copyright}>© Aucun droit réservé - 2022</div>
       </footer>

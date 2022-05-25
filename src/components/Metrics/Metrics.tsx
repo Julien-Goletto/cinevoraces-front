b61568ed26bd5fbb059a6f66eff4bd6c49815a62
benoit safari <gregory.michalak.192@gmail.com>
2022-05-26 00:08:52 +0200

    <fix> metrics no longuer crash app when back is not reachable + hopefully fixed .env for good

--- src/components/Metrics/Metrics.tsx
+++ src/components/Metrics/Metrics.tsx
@@ -2,29 +2,30 @@ import { useAllMetricsQuery } from 'redux/api';
 import styles from './Metrics.module.scss';
 
 function Metrics() {
-  const { data , isLoading } = useAllMetricsQuery<Metrics>();
-  
-  if (!isLoading) {
-    return (
-      <section className={styles.metrics}>
-        <h3 className={styles.title}>CinéVoraces en quelques chiffres</h3>
-        <div className={styles.grid}>
-          <div className={`${styles.metric} ${styles['metric--purple']}`}>
-            <span className={styles.count}>{data.seasons_count}</span>
-            <span className={styles.type}>Saisons</span>
+  const { data } = useAllMetricsQuery<Metrics>();  
+  return (
+    <>
+      { data &&
+        <section className={styles.metrics}>
+          <h3 className={styles.title}>CinéVoraces en quelques chiffres</h3>
+          <div className={styles.grid}>
+            <div className={`${styles.metric} ${styles['metric--purple']}`}>
+              <span className={styles.count}>{data.seasons_count}</span>
+              <span className={styles.type}>Saisons</span>
+            </div>
+            <div className={`${styles.metric} ${styles['metric--yellow']}`}>
+              <span className={styles.count}>{data.movies_count}</span>
+              <span className={styles.type}>Films</span>
+            </div>
+            <div className={`${styles.metric} ${styles['metric--green']}`}>
+              <span className={styles.count}>{data.countries_count}</span>
+              <span className={styles.type}>Pays</span>
+            </div>
           </div>
-          <div className={`${styles.metric} ${styles['metric--yellow']}`}>
-            <span className={styles.count}>{data.movies_count}</span>
-            <span className={styles.type}>Films</span>
-          </div>
-          <div className={`${styles.metric} ${styles['metric--green']}`}>
-            <span className={styles.count}>{data.countries_count}</span>
-            <span className={styles.type}>Pays</span>
-          </div>
-        </div>
-      </section>
-    );
-  } else return null;
+        </section>
+      }
+    </>
+  );
 };
 
 export default Metrics;

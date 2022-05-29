ea374bc3e64296b04495dbc14bd3e6e6d1c4ac16
benoit safari <gregory.michalak.192@gmail.com>
2022-05-29 22:54:46 +0200

    <feature> testing img upload

--- src/pages/User/UserParams/UserPicUploader.tsx
+++ src/pages/User/UserParams/UserPicUploader.tsx
@@ -1,4 +1,5 @@
 import { Button } from 'components/Buttons/Button';
+import { timeStamp } from 'console';
 import { useState } from 'react';
 import styles from './UserParams.module.scss';
 
@@ -6,18 +7,34 @@ function UserPicUploader ({avatar}: {avatar: string}) {
   const [ isUpdateOpen, setIsUpdateOpen ] = useState(false);
   const [ image, setImage ]               = useState<any>('');
   const [ preview, setPreview ]           = useState<any>(undefined);
+  // const [ url, setUrl ] = useState("");
 
-  const uploadImage = () => {
-  };
+  const uploadImage = () => {};
   const onChangeHandler = (e: any) => {
     const [file] = e.target.files;
-    setImage(file);
-    setPreview(URL.createObjectURL(file));
+    // const eagerParams = 'c_crop,w_200,h_200,g_faces';
+    // const folder = '';
+    // setImage(file);
+    // setPreview(URL.createObjectURL(file));
+    const data = new FormData();
+    // data.append('timestamp', timeStamp);
+    // data.append('signature', signature);
+    // data.append('api_key', process.env.REACT_APP_CLOUD_API_KEY!);
+    // data.append('eager', eagerParams);
+    // data.append('folder', folder);
+    data.append('file', file);
+    data.append('upload_preset', process.env.REACT_APP_CLOUD_PRESET!);
+    data.append('cloud_name', process.env.REACT_APP_CLOUD_NAME!);
+    fetch(`${process.env.REACT_APP_CLOUD_URL}/${process.env.REACT_APP_CLOUD_NAME}/image/upload`, { method: 'post', body: data })
+      .then(resp => resp.json())
+      .then(data => {setPreview(data.url);})
+      .catch(err => console.log(err)
+      );
   };
   const openUpdateHandler = () => {
     setIsUpdateOpen(!isUpdateOpen);
   };
-  
+
   return (
     <>
       { isUpdateOpen &&

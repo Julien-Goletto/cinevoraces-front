f46619314f43ead52324ed869124862a9890a117
BenoitSafari <gregory.michalak.192@gmail.com>
2022-06-22 15:43:41 +0200

    <feat> Added fixed version of InputStar

--- src/components/Inputs/InputStar.tsx
+++ src/components/Inputs/InputStar.tsx
@@ -13,23 +13,34 @@ type InputStarProps = {
  * @param setter    setter function (only for input)
  */
 function InputStar ({isInput, value, setter}: InputStarProps) {
+  // Create radio inputs index (reversed is CSS related)
   const array: number[] = [5,4,3,2,1];      
   const handleOnChange = (e: React.FormEvent<HTMLInputElement>) => {
     setter!(Number(e.currentTarget.value));
   }; 
+
   return(
     <div className={styles['input-star']}>
-      {array.map((el) => (
-        <input
-          className={styles.input}
-          type='radio'
-          value={el}
-          key={el}
-          name='input-star'
-          onChange={handleOnChange}
-          defaultChecked={(value === el) ? true : false}
-        />
-      ))}
+      {isInput &&
+        // Interactive version
+        array.map(el => (
+          <input
+            className={styles.input}
+            type='radio'
+            value={el}
+            key={el}
+            name='input-star'
+            onChange={handleOnChange}
+            defaultChecked={(value === el) ? true : false}
+          />
+        ))}
+      {!isInput &&
+        // Fixed version
+        array.map(el => (
+          <div 
+            key={el}
+            className={(el > value) ? styles.star : `${styles.star} ${styles['star--checked']}`}></div>
+        ))}
     </div>
   );
 }

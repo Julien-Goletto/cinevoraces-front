17b72bbdddb25bc8feee3b0955b7d1b6838eac7b
BenoitSafari <gregory.michalak.192@gmail.com>
2022-06-22 18:20:49 +0200

    <ref> Added useEffect

--- src/components/Inputs/InputStar.tsx
+++ src/components/Inputs/InputStar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import styles from './InputStar.module.scss';
 
 type InputStarProps = {
@@ -13,33 +13,41 @@ type InputStarProps = {
  * @param setter    setter function (only for input)
  */
 function InputStar ({isInput, value, setter}: InputStarProps) {
-  // Create radio inputs index (reversed is CSS related)
-  const array: number[] = [5,4,3,2,1];      
+  // Create radio inputs index
+  type starIndex = {value: number, defaultChecked: boolean}[];
+  const [starArray, setStarArray] = useState<starIndex>([]);
+  useEffect(() => {
+    const array: starIndex = [];
+    // Invert index order for CSS '~' selector
+    for (let i = 5; i !== 0; i--) {
+      array.push({value: i, defaultChecked: (value === i) && true});
+    }
+    setStarArray(array);
+  }, [value]);
   const handleOnChange = (e: React.FormEvent<HTMLInputElement>) => {
     setter!(Number(e.currentTarget.value));
-  }; 
-
+  };
   return(
     <div className={styles['input-star']}>
       {isInput &&
         // Interactive version
-        array.map(el => (
+        starArray.map(({value, defaultChecked}) => (
           <input
             className={styles.input}
             type='radio'
-            value={el}
-            key={el}
+            value={value}
+            key={value}
             name='input-star'
             onChange={handleOnChange}
-            defaultChecked={(value === el) ? true : false}
+            defaultChecked={defaultChecked}
           />
         ))}
       {!isInput &&
         // Fixed version
-        array.map(el => (
+        starArray.map(el => (
           <div 
-            key={el}
-            className={(el > value) ? styles.star : `${styles.star} ${styles['star--checked']}`}></div>
+            key={el.value}
+            className={(el.value > value) ? styles.star : `${styles.star} ${styles['star--checked']}`}></div>
         ))}
     </div>
   );

b06f283c79cc3765e1eda2979c4d8f93f7526580
BenoitSafari <gregory.michalak.192@gmail.com>
2022-06-23 23:53:26 +0200

    adapted component to new state

--- src/pages/Films/MoviesGrid/MoviesGrid.tsx
+++ src/pages/Films/MoviesGrid/MoviesGrid.tsx
@@ -1,44 +1,22 @@
-import { useState } from 'react';
-import { Link     } from 'react-router-dom';
-import Loader from 'components/Loader/Loader';
-import styles from './MoviesGrid.module.scss';
 import { motion } from 'framer-motion';
+import { Link } from 'react-router-dom';
+import styles from './MoviesGrid.module.scss';
 
-function MoviesGrid({ movies, isLoading }: MovieGrid) {
-  const [isImgLoading, setIsImgLoading] = useState(true);
-  const onLoadHandler = () => {
-    setTimeout(() => {setIsImgLoading(false);}, 3000);
-  };
+function MoviesGrid({movies}: {movies: DBMovie[]}) {
 
   return(
-    !isLoading ?
-      <motion.div
-        className={styles.grid}
-        animate={{ 
-          opacity: [0,1]
-        }}
-        transition= {{delay: 0.2}}
-      >
-        {
-          movies.map((movie:any) => 
-            <motion.div 
-              className={styles.poster}
-              key={movie.id}
-            >
-              <Link to={`/film/${movie.id}`}>
-                <img 
-                  className={`${styles.img}`}
-                  src={movie.poster_url} alt={`Affiche du film ${movie.title}`}
-                  onLoad={onLoadHandler}
-                />
-              </Link>
-            </motion.div>
-          )}
-      </motion.div>
-      : 
-      <div className={styles['loader-wrapper']}>
-        <Loader />
-      </div>
+    <motion.div className={styles.grid} 
+      animate={{opacity: [0,1]}}
+      transition= {{delay: 0.2}}
+    >
+      {movies.map((movie:any) => 
+        <motion.div className={styles.poster} key={movie.id}>
+          <Link to={`/film/${movie.id}`}>
+            <img className={`${styles.img}`} src={movie.poster_url} alt=''/>
+          </Link>
+        </motion.div>
+      )}
+    </motion.div>
   );
 };
 

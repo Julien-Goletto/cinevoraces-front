5b518b9e5987e97f43d6364630c67e692cc0c0e3
BenoitSafari <gregory.michalak.192@gmail.com>
2022-06-23 18:52:16 +0200

    <ref> Adapted Filters to new Inputs

--- src/pages/Films/Filters/Filters.tsx
+++ src/pages/Films/Filters/Filters.tsx
@@ -1,29 +1,45 @@
 import { useState } from 'react';
 import { useAppSelector, useAppDispatch } from 'redux/hooks';
-import { getQuery, setQuery, filters, setSeasonFilter, setTagFilter } from 'redux/slices/filter';
-import { Button, InputText } from 'components/Inputs/InputsLib';
+import { 
+  filters, 
+  setSeasonFilter,
+  setQuery, 
+  setTagFilter,
+  setPeriodeMinVal,
+  setPeriodeMaxVal
+} from 'redux/slices/filter';
+import { Button, InputText, InputCheckbox, InputRadio, DoubleInputRange } from 'components/Inputs/InputsLib';
 import { FilterMenu, DropDown } from './FilterMenu';
 import styles from './Filters.module.scss';
 
 function Filters() {
-  const [isDropMenu, SetIsDropMenu] = useState(false);
+  const [isFilterMenu, setFilterMenu] = useState(false);
   const dispatch = useAppDispatch();
-  const query = useAppSelector(getQuery);
-
-  // Handlers
-  const handleDropDownState = () => {
-    (isDropMenu) ? SetIsDropMenu(false) : SetIsDropMenu(true);
+  const { query, seasons, tags, periode } = useAppSelector(filters);
+  const handleFilterMenu = () => {
+    (isFilterMenu) ? setFilterMenu(false) : setFilterMenu(true);
   };
-  const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const value = event.target.value;
-    dispatch(setQuery(value));
+  const handleSeasonSetter = (event: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(setSeasonFilter(event.target.value));
+  };
+  const handleTagSetter = (event: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(setTagFilter(event.target.value));
+  };
+  const handleMinPeriodeSetter = (value: number) => {
+    dispatch(setPeriodeMinVal(value));
+  };
+  const handleMaxPeriodeSetter = (value: number) => {
+    dispatch(setPeriodeMaxVal(value));
+  };
+  const handleSearchBarQuery = (event: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(setQuery(event.target.value));
   };
   
   return(
     <div className={styles.filters}>
-      <Button handler={handleDropDownState}> 
+      <Button handler={handleFilterMenu}> 
         Filtrer
-        { (isDropMenu) ? <img src='/images/filter-open.svg' alt='' /> : <img src='/images/filter-closed.svg' alt='' /> }
+        { (isFilterMenu) ? <img src='/images/filter-open.svg' alt='' /> : <img src='/images/filter-closed.svg' alt='' /> }
       </Button>
       <div className={styles['search-bar']}>
         <InputText 
@@ -32,13 +48,46 @@ function Filters() {
           type='text'
           placeholder='Recherche un film'
           value={query}
-          handler={handleOnChange}
+          handler={handleSearchBarQuery}
         />
       </div>
-      { isDropMenu && 
-        <FilterMenu handleClose={handleDropDownState}>
-          <DropDown name='Genres'>
-            Jambon
+      { isFilterMenu && 
+        <FilterMenu handleClose={handleFilterMenu}>
+          <DropDown name={'Saison'}>
+            { seasons.map(({name, value, isChecked}) => (
+              <InputRadio
+                name={name}
+                isChecked={isChecked}
+                value={value}
+                handler={handleSeasonSetter}
+                field='season'
+              />
+            ))}
+          </DropDown>
+          {/* FIXME: setTagFilter not working, will be fixed with filter.ts refactoring */}
+          { tags.map(({tagName, tags}) => (
+            <DropDown name={tagName}>
+              { tags.map(({name, isChecked}, index) => (
+                <li key={index}>
+                  <InputCheckbox
+                    name={name}
+                    isChecked={isChecked}
+                    handler={handleTagSetter}
+                  />
+                </li>
+              ))}
+            </DropDown>
+          ))}
+          <DropDown name={'Période'}>
+            <DoubleInputRange
+              min={periode.baseValues[0]}
+              max={periode.baseValues[1]}
+              valueMin={periode.stateValues[0]}
+              valueMax={periode.stateValues[1]}
+              minSetter={handleMinPeriodeSetter}
+              maxSetter={handleMaxPeriodeSetter}
+              label='Période'
+            />
           </DropDown>
         </FilterMenu>
       }

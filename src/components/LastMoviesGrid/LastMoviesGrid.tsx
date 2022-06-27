5bccd4e96d08b583969c6c35d8c44df75e835378
BenoitSafari <gregory.michalak.192@gmail.com>
2022-06-27 10:06:00 +0200

    <ref> removed useEffect

--- src/components/LastMoviesGrid/LastMoviesGrid.tsx
+++ src/components/LastMoviesGrid/LastMoviesGrid.tsx
@@ -9,42 +9,31 @@ import styles from './LastMoviesGrid.module.scss';
  * @return grid with the last 5 movies posted
  */
 function LastMoviesGrid() {
-  const {data, isLoading} = useAllMoviesQuery('');
-  const [lastMovies, setLastMovies] = useState<DBMovie[]>();
-
-  useEffect(()=> {
-    (!isLoading && data) && setLastMovies([...data].slice(0,5));
-  }, [data, isLoading]);
+  const {data: movies, isLoading} = useAllMoviesQuery('');
 
   return(
-    (lastMovies && !isLoading) ?
-      <div id='last-movie' className={styles['last-movies']}>
-        <h2 className={styles.title}>Les derniers ajouts de la communauté</h2>
-        <div className={styles.grid}>
-          {lastMovies.map(({poster_url, french_title, id}) => 
-            <div className={styles.poster} key={id}>
-              <Link to={`/film/${id}`}>
-                <img 
-                  className={styles.img}
-                  src={poster_url} 
-                  alt={''}
-                />
-              </Link>
-            </div>
-          )}
-        </div>
-
-        <Button
-          href='/films'
-          styleMod='fill-rounded'
-        >
-          Voir la liste des films
-        </Button>
-      </div> 
-      :
-      <div id='last-movie' className={styles.loader}>
-        <Loader />
-      </div>
+    <>
+      { !isLoading && movies &&
+        <div id='last-movie' className={styles['last-movies']}>
+          <h2 className={styles.title}>Les derniers ajouts de la communauté</h2>
+          <div className={styles.grid}>
+            {/* Map only last 5 movies */}
+            {[...movies].slice(0,5).map(({poster_url, id}) => 
+              <div className={styles.poster} key={id}>
+                <Link to={`/film/${id}`}>
+                  <img className={styles.img} src={poster_url} alt={''}/>
+                </Link>
+              </div>)}
+          </div>
+          <Button href='/films' styleMod='fill-rounded'>
+            Voir la liste des films
+          </Button>
+        </div>}
+      { isLoading &&
+        <div id='last-movie' className={styles.loader}>
+          <Loader />
+        </div>}
+    </>
   );
 }
 

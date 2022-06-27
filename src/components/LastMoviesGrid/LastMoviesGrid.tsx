64bba20c63ad778abd35c22591adf0c6e9d11313
BenoitSafari <gregory.michalak.192@gmail.com>
2022-06-27 10:08:09 +0200

    <ref> removed unused dependencies

--- src/components/LastMoviesGrid/LastMoviesGrid.tsx
+++ src/components/LastMoviesGrid/LastMoviesGrid.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import { useAllMoviesQuery } from 'redux/api';
 import { Link } from 'react-router-dom';
 import { Button } from 'components/Inputs/InputsLib';

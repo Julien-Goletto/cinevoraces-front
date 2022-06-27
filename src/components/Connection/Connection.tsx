7521b08eb85bb7f95bbcfd6be2ead650d0fdfb72
BenoitSafari <gregory.michalak.192@gmail.com>
2022-06-27 13:30:51 +0200

    <ref> Moved Connection component

---
+++ src/components/Connection/Connection.tsx
@@ -0,0 +1,60 @@
+import Loader from 'components/Loader/Loader';
+import { Button, InputText } from 'components/Inputs/InputsLib';
+import { useAppDispatch } from 'redux/hooks';
+import { useUserLoginMutation } from 'redux/api';
+import { addToast, toggleModal } from 'redux/slices/global';
+import { setUser } from 'redux/slices/user';
+import { useEffect } from 'react';
+import styles from './Connection.module.scss';
+
+/**
+ * @returns Connection form
+ */
+function Connection() {
+  const [loginUser, {data: userData, isSuccess, isLoading}] = useUserLoginMutation();
+  const dispatch = useAppDispatch();
+
+  const sendForm = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const form = new FormData(e.currentTarget);
+    const user = {      
+      pseudo: form.get('username'),
+      password: form.get('password')
+    };   
+    loginUser(user);
+  };
+  const handleModal = () => {
+    dispatch(toggleModal());
+  };
+
+  useEffect(()=> {
+    if(isSuccess) {
+      dispatch(setUser(userData));
+      dispatch(toggleModal());
+      dispatch(addToast({type:'success', text: `Bienvenue ${userData!.pseudo}`}));
+    }
+  }, [userData, isSuccess, dispatch]);
+
+  
+  return (
+    <div className={styles.container}> 
+      { isLoading &&
+        <Loader isMaxed/>}
+      <form onSubmit={sendForm}>
+        <InputText label="Nom d'utilisateur" name='username' type='text' placeholder='Entrez votre nom d’utilisateur'/>
+        <InputText label='Mot de passe' name='password' type='password' placeholder='Entrez votre mot de passe'/>
+        <div className={styles.button}>
+          <Button styleMod='fill'>
+            Se connecter
+          </Button>
+          <Button href='/register' handler={handleModal}>
+            S'inscrire
+          </Button>
+        </div>
+      </form>
+
+    </div>
+  );
+}
+
+export default Connection;

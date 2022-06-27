42c771efab5271803d81443607ee296b262030a9
BenoitSafari <gregory.michalak.192@gmail.com>
2022-06-27 14:02:51 +0200

    <doc> Added comments

--- src/components/Connection/Connection.tsx
+++ src/components/Connection/Connection.tsx
@@ -1,10 +1,10 @@
-import Loader from 'components/Loader/Loader';
-import { Button, InputText } from 'components/Inputs/InputsLib';
+import { useEffect } from 'react';
 import { useAppDispatch } from 'redux/hooks';
 import { useUserLoginMutation } from 'redux/api';
 import { addToast, toggleModal } from 'redux/slices/global';
 import { setUser } from 'redux/slices/user';
-import { useEffect } from 'react';
+import { Button, InputText } from 'components/Inputs/InputsLib';
+import Loader from 'components/Loader/Loader';
 import styles from './Connection.module.scss';
 
 /**
@@ -14,24 +14,27 @@ function Connection() {
   const [loginUser, {data: userData, isSuccess, isLoading}] = useUserLoginMutation();
   const dispatch = useAppDispatch();
 
-  const sendForm = async (e: React.FormEvent<HTMLFormElement>) => {
+  // Login handler function
+  const login = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const form = new FormData(e.currentTarget);
-    const user = {      
+    loginUser({      
       pseudo: form.get('username'),
       password: form.get('password')
-    };   
-    loginUser(user);
+    });
   };
   const handleModal = () => {
     dispatch(toggleModal());
   };
 
+  // Handle connection success
   useEffect(()=> {
     if(isSuccess) {
-      dispatch(setUser(userData));
-      dispatch(toggleModal());
-      dispatch(addToast({type:'success', text: `Bienvenue ${userData!.pseudo}`}));
+      dispatch(setUser(userData)); // Update local user state
+      dispatch(toggleModal());  // Close modal
+      dispatch(addToast(
+        // Show success message to user
+        {type:'success', text: `Bienvenue ${userData!.pseudo}`}));
     }
   }, [userData, isSuccess, dispatch]);
 
@@ -40,7 +43,7 @@ function Connection() {
     <> 
       { isLoading &&
         <Loader isMaxed/>}
-      <form className={styles.connection} onSubmit={sendForm}>
+      <form className={styles.connection} onSubmit={login}>
         <InputText label="Nom d'utilisateur" name='username' type='text' placeholder='Entrez votre nom d’utilisateur'/>
         <InputText label='Mot de passe' name='password' type='password' placeholder='Entrez votre mot de passe'/>
         <div className={styles['button-container']}>

a742d5c95e75c77febad77b9848f99b7fd0f1adc
benoitsafari <gregory.michalak.192@gmail.com>
2022-07-07 00:18:01 +0200

    <ref> added loader for refresh token await

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,16 @@ import Cookies     from 'js-cookie';
 import Toast       from 'components/Toasts/Toasts';
 import ResetScroll from 'components/ResetScroll/ResetScroll';
 import Layout      from 'components/Layout/Layout';
-import Error       from './pages/Error/Error';
-import Films       from './pages/Films/Films';
-import User        from './pages/User/User';
-import Home        from './pages/Home/Home';
-import Film        from './pages/Film/Film';
-import Register    from './pages/Register/Register';
-import Proposal    from './pages/Proposal/Proposal';
-import Team        from './pages/Team/Team';
-import Admin       from './pages/Admin/Admin';
+import Loader      from 'components/Loader/Loader';
+import Error       from 'pages/Error/Error';
+import Films       from 'pages/Films/Films';
+import User        from 'pages/User/User';
+import Home        from 'pages/Home/Home';
+import Film        from 'pages/Film/Film';
+import Register    from 'pages/Register/Register';
+import Proposal    from 'pages/Proposal/Proposal';
+import Team        from 'pages/Team/Team';
+import Admin       from 'pages/Admin/Admin';
 
 function App() {
   const dispatch = useAppDispatch();
@@ -33,26 +34,31 @@ function App() {
     (!isLoading && data && !isError) && dispatch(login(data));
   }, [data]);
   
-  return (
+  return ( 
     <>
-      <Layout>
-        <AnimatePresence exitBeforeEnter>
-          <ResetScroll />
-          <Routes location={location} key={location.pathname}>
-            <Route path='/' element={<Home />}/>
-            <Route path='/film/:id' element={<Film />}/>
-            <Route path='/films' element={<Films />}/>
-            <Route path='/register' element={<Register />}/>
-            <Route path='/user/:id' element={<User />}/>
-            <Route path='/proposal' element={<Proposal />}/>
-            <Route path='/team' element={<Team />}/>
-            <Route path='/admin' element={<Admin />}/>
-            <Route path='*' element={<Error />}/>
-          </Routes>
-        </AnimatePresence>
-      </Layout>
-      <Toast />
+      {data &&
+        <>
+          <Layout>
+            <AnimatePresence exitBeforeEnter>
+              <ResetScroll />
+              <Routes location={location} key={location.pathname}>
+                <Route path='/' element={<Home />}/>
+                <Route path='/film/:id' element={<Film />}/>
+                <Route path='/films' element={<Films />}/>
+                <Route path='/register' element={<Register />}/>
+                <Route path='/user/:id' element={<User />}/>
+                <Route path='/proposal' element={<Proposal />}/>
+                <Route path='/team' element={<Team />}/>
+                <Route path='/admin' element={<Admin />}/>
+                <Route path='*' element={<Error />}/>
+              </Routes>
+            </AnimatePresence>
+          </Layout>
+          <Toast />
+        </>}
+      {!data && <Loader isMaxed/>}
     </>
+
   );
 }
 

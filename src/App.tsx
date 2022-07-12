39312e11d0c0c1c9c819e3c06ec904ff583c8ad3
benoitsafari <gregory.michalak.192@gmail.com>
2022-07-12 23:58:56 +0200

    <fix> fixed refreshToken useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { useEffect }                  from 'react';
-import { Routes, Route, useLocation } from 'react-router-dom';
-import { AnimatePresence }            from 'framer-motion';
-import { useLazyRefreshTokenQuery }   from 'redux/api';
-import { useAppDispatch }             from 'redux/hooks';
-import { login }                      from 'redux/slices/user';
+import { useEffect, useState }            from 'react';
+import { Routes, Route, useLocation }     from 'react-router-dom';
+import { AnimatePresence }                from 'framer-motion';
+import { useLazyRefreshTokenQuery }       from 'redux/api';
+import { useAppDispatch, useAppSelector } from 'redux/hooks';
+import { login, userState }               from 'redux/slices/user';
 import Cookies     from 'js-cookie';
 import Toast       from 'components/Toasts/Toasts';
 import ResetScroll from 'components/ResetScroll/ResetScroll';
@@ -16,7 +16,7 @@ import Home        from 'pages/Home/Home';
 import Film        from 'pages/Film/Film';
 import Register    from 'pages/Register/Register';
 import Proposal    from 'pages/Proposal/Proposal';
-import About        from 'pages/About/About';
+import About       from 'pages/About/About';
 import Admin       from 'pages/Admin/Admin';
 
 function App() {
@@ -28,7 +28,7 @@ function App() {
   useEffect(() => {
     // Request new token
     Cookies.get('refreshToken') && refreshToken();
-  }, []);
+  }, [refreshToken]);
   useEffect(() => {
     // Dispatch response
     (!isLoading && data && !isError) && dispatch(login(data));
@@ -55,9 +55,8 @@ function App() {
           </AnimatePresence>
           <Toast/>
         </>}
-      {(isLoading) && <Loader isMaxed/>}
+      {isLoading && <Loader isMaxed/>}
     </>
-
   );
 }
 
